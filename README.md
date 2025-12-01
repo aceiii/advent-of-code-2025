cc9eaa30fb349e1b919b427c93fa86e28961286a
Borin Ouch <borin.ouch@gmail.com>
2025-11-30 20:56:49 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2025
+
+ - https://adventofcode.com/2025/
+ - https://github.com/aceiii/advent-of-code-2025
+
---
+++ python/template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    lines = sys.stdin.read().strip().split("\n")
+    print("Part1: {}".format(part1(lines)))
+    print("Part2: {}".format(part2(lines)))
+
+
+if __name__ == "__main__":
+    main()
+
