d492635b3e6f058a157cece698075d7520960ab8
Borin Ouch <borin.ouch@gmail.com>
2025-12-12 08:48:17 -0800

    Solve day11 part 2

--- python/day11.py
+++ python/day11.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+from math import prod
 from collections import defaultdict
 
 
@@ -12,31 +13,145 @@ def parse_devices(lines):
     return devices
 
 
-def count_devices(ports, dev):
-    print('dev',  dev)
-    if dev == 'you':
-        return 1
-    return sum(count_devices(ports, d) for d in ports[dev])
+def count_paths(devices, start, end):
+    visits = defaultdict(lambda: 0)
+    visits[start] = 1
+    q = [start]
+    while q:
+        dev = q.pop()
+        #n = visits[dev]
+        for port in devices[dev]:
+            q.append(port)
+            visits[port] += 1
+    return visits[end]
 
 
 def part1(lines):
     devices = parse_devices(lines)
-    visits = defaultdict(lambda: 0)
-    visits['you'] = 1
-    q = ['you']
+    return count_paths(devices, 'you', 'out')
+
+
+def reverse_devices(devices):
+    rdevices = defaultdict(lambda: set())
+    for dev, ports in devices.items():
+        for port in ports:
+            rdevices[port].add(dev)
+    return rdevices
+
+
+def count_parents(devices, start, end, cache=None):
+    if end == start:
+        return 1
+
+    if cache is None:
+        cache = {}
+    if start in cache:
+        return cache[start]
+
+    count = sum(count_parents(devices, dev, end, cache) for dev in devices[start])
+    cache[start] = count
+    return count
+
+
+def topsort(edges):
+    arr = []
+    s = []
+
+    incoming = defaultdict(lambda: [])
+    outgoing = defaultdict(lambda: [])
+    nodes = set()
+    for a, b in edges:
+        incoming[b].append(a)
+        outgoing[a].append(b)
+        nodes.add(a)
+        nodes.add(b)
+
+    for n in nodes:
+        if n not in incoming or not incoming[n]:
+            s.append(n)
+
+    while s:
+        n = s.pop()
+        arr.append(n)
+        for n2 in outgoing[n]:
+            incoming[n2].remove(n)
+            if not incoming[n2]:
+                s.append(n2)
+
+    return arr
+
+
+def count_paths2(devices, start, end):
+    graph = defaultdict(lambda: [])
+    indegree = defaultdict(lambda: 0)
+
+    edges = set()
+    for dev, ports in devices.items():
+        for port in ports:
+            edges.add((dev, port))
+
+    for u, v in edges:
+        graph[u].append(v)
+        indegree[v] += 1
+
+    q = []
+    for i, n in indegree.items():
+        if n == 0:
+            q.append(i)
+
+    top_order = []
     while q:
-        dev = q.pop(0)
+        node = q.pop(0)
+        top_order.append(node)
+        for neighbor in graph[node]:
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                q.append(neighbor)
+
+    ways = defaultdict(lambda: 0)
+    ways[start] = 1
+
+    for node in top_order:
+        for neighbor in graph[node]:
+            ways[neighbor] += ways[node]
+
+    return ways[end]
+
+
+def get_edges(devices):
+    edges = []
+    for dev, out in devices.items():
+        for dev2 in out:
+            edges.append((dev, dev2))
+    return edges
+
+
+def count_paths3(devices, start, end):
+    sorted_dev = topsort(get_edges(devices))
+    visits = defaultdict(lambda: 0)
+    visits[start] = 1
+    while sorted_dev:
+        dev = sorted_dev.pop(0)
         n = visits[dev]
         for port in devices[dev]:
-            if port not in visits:
-                q.append(port)
             visits[port] += n
-            print('port', port, '=>',visits[port])
-    return visits['out']
+    return visits[end]
 
 
 def part2(lines):
-    pass
+    devices = parse_devices(lines)
+    a = [
+        count_paths3(devices, 'svr', 'dac'),
+        count_paths3(devices, 'dac', 'fft'),
+        count_paths3(devices, 'fft', 'out'),
+    ]
+
+    b = [
+        count_paths3(devices, 'svr', 'fft'),
+        count_paths3(devices, 'fft', 'dac'),
+        count_paths3(devices, 'dac', 'out'),
+    ]
+    return prod(a) + prod(b)
 
 
 def main():
