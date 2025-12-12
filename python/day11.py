976daf5b9aeffffe56c932fa269d5ccfaae11397
Borin Ouch <borin.ouch@gmail.com>
2025-12-12 08:51:03 -0800

    Clean up day11

--- python/day11.py
+++ python/day11.py
@@ -13,46 +13,6 @@ def parse_devices(lines):
     return devices
 
 
-def count_paths(devices, start, end):
-    visits = defaultdict(lambda: 0)
-    visits[start] = 1
-    q = [start]
-    while q:
-        dev = q.pop()
-        #n = visits[dev]
-        for port in devices[dev]:
-            q.append(port)
-            visits[port] += 1
-    return visits[end]
-
-
-def part1(lines):
-    devices = parse_devices(lines)
-    return count_paths(devices, 'you', 'out')
-
-
-def reverse_devices(devices):
-    rdevices = defaultdict(lambda: set())
-    for dev, ports in devices.items():
-        for port in ports:
-            rdevices[port].add(dev)
-    return rdevices
-
-
-def count_parents(devices, start, end, cache=None):
-    if end == start:
-        return 1
-
-    if cache is None:
-        cache = {}
-    if start in cache:
-        return cache[start]
-
-    count = sum(count_parents(devices, dev, end, cache) for dev in devices[start])
-    cache[start] = count
-    return count
-
-
 def topsort(edges):
     arr = []
     s = []
@@ -81,43 +41,6 @@ def topsort(edges):
     return arr
 
 
-def count_paths2(devices, start, end):
-    graph = defaultdict(lambda: [])
-    indegree = defaultdict(lambda: 0)
-
-    edges = set()
-    for dev, ports in devices.items():
-        for port in ports:
-            edges.add((dev, port))
-
-    for u, v in edges:
-        graph[u].append(v)
-        indegree[v] += 1
-
-    q = []
-    for i, n in indegree.items():
-        if n == 0:
-            q.append(i)
-
-    top_order = []
-    while q:
-        node = q.pop(0)
-        top_order.append(node)
-        for neighbor in graph[node]:
-            indegree[neighbor] -= 1
-            if indegree[neighbor] == 0:
-                q.append(neighbor)
-
-    ways = defaultdict(lambda: 0)
-    ways[start] = 1
-
-    for node in top_order:
-        for neighbor in graph[node]:
-            ways[neighbor] += ways[node]
-
-    return ways[end]
-
-
 def get_edges(devices):
     edges = []
     for dev, out in devices.items():
@@ -126,7 +49,7 @@ def get_edges(devices):
     return edges
 
 
-def count_paths3(devices, start, end):
+def count_paths(devices, start, end):
     sorted_dev = topsort(get_edges(devices))
     visits = defaultdict(lambda: 0)
     visits[start] = 1
@@ -138,18 +61,23 @@ def count_paths3(devices, start, end):
     return visits[end]
 
 
+def part1(lines):
+    devices = parse_devices(lines)
+    return count_paths(devices, 'you', 'out')
+
+
 def part2(lines):
     devices = parse_devices(lines)
     a = [
-        count_paths3(devices, 'svr', 'dac'),
-        count_paths3(devices, 'dac', 'fft'),
-        count_paths3(devices, 'fft', 'out'),
+        count_paths(devices, 'svr', 'dac'),
+        count_paths(devices, 'dac', 'fft'),
+        count_paths(devices, 'fft', 'out'),
     ]
 
     b = [
-        count_paths3(devices, 'svr', 'fft'),
-        count_paths3(devices, 'fft', 'dac'),
-        count_paths3(devices, 'dac', 'out'),
+        count_paths(devices, 'svr', 'fft'),
+        count_paths(devices, 'fft', 'dac'),
+        count_paths(devices, 'dac', 'out'),
     ]
     return prod(a) + prod(b)
 
